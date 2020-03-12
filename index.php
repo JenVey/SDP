546504af6cb500c76f6de214fe4a41047d82130c
robbygiovanni <42507991+robbygiovanni@users.noreply.github.com>
2020-03-12 14:33:06 +0700

    new

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gather.Owl</title>
+    <link rel="stylesheet" href="CSS/bootstrap.css">
+    <link rel="stylesheet" href="CSS/Ours.css">
+</head>
+<body>
+    <nav class="navbar Balok">
+    <a class="navbar-brand CompName" style="color: #D7C13F;">gather.owl</a>
+    <form class="form-inline">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+    </nav>
+</body>
+</html>
