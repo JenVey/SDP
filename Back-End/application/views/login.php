b7983d4cfe0ae024dce7e5c8ece21d194642ef80
robbygiovanni <42507991+robbygiovanni@users.noreply.github.com>
2020-03-15 02:34:35 +0700

    oke

--- Back-End/application/views/login.php
+++ Back-End/application/views/login.php
@@ -54,19 +54,11 @@
                 <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
             </div>
             </div>
-            <div class="form-group row">
-            <div class="offset-sm-2 col-sm-10">
-                <div class="form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck2">
-                <label class="form-check-label" for="exampleCheck2">Remember me</label>
-                </div>
-            </div>
-            </div>
         </div>
         <!-- /.card-body -->
         <div class="card-footer">
-            <button type="submit" href="<?= base_url(); ?>home" class="btn btn-info">Sign in</button>
-            <button type="submit" class="btn btn-default float-right">Cancel</button>
+            <a type="button" href="<?= base_url(); ?>home" class="btn btn-info">Sign in</a>
+            <a type="button" class="btn btn-default float-right">Cancel</a>
         </div>
         <!-- /.card-footer -->
         </form>
